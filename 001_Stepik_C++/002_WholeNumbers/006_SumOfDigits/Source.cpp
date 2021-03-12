bc82e2788c6e714bee8a8395f6c9d951014a3bb5
Artem <nik.artem.larin@gmail.com>
2021-03-12 21:14:43 +0600

    добавил SumOfDigits

---
+++ 001_Stepik_C++/002_WholeNumbers/006_SumOfDigits/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int number;
+
+	cout << "Введите трехзначное число: ";
+	cin >> number;
+
+	int hundreds = number / 100; // сотни
+	int dozens = number % 100 / 10; // десятки
+	int units = number % 10; // единицы
+
+	cout << "Общая сумма всех чисел в отдельности данного числа " << number;
+	cout << ": " << hundreds + dozens + units << endl << endl;
+
+	cout << "Готово!" << endl;
+	cout << "Программа завершена." << endl;
+
+	return 0;
+}
