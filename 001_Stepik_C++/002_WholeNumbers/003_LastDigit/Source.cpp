30dbef6b8bd52e42156ef84764064e68ecb64296
Artem <nik.artem.larin@gmail.com>
2021-03-12 21:12:42 +0600

    добавил LastDigit

---
+++ 001_Stepik_C++/002_WholeNumbers/003_LastDigit/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int number;
+
+	cout << "Введите число: ";
+	cin >> number;
+
+	cout << "Последняя цифра числа " << number;
+	cout << " это: " << number % 10 << endl << endl;
+
+	cout << "Готово!" << endl;
+	cout << "Программа завершена." << endl;
+
+	return 0;
+}
