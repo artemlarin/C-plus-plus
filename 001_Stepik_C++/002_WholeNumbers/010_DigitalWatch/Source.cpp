223e7175867ad4bc78f97b37fc1c30e8155ad895
Artem <nik.artem.larin@gmail.com>
2021-03-12 21:17:17 +0600

    добавил DigitalWatch

---
+++ 001_Stepik_C++/002_WholeNumbers/010_DigitalWatch/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int seconds;
+
+	cout << "Введите колличество секунд: ";
+	cin >> seconds;
+
+	cout << endl;
+
+	int hour = seconds / 3600 % 24; /* часы */
+	int minute = seconds / 60 % 60; /* минуты */
+	int second = seconds % 60; /* секунды */
+
+	/* вывод времени в формате h:mm:ss */
+	cout << "Преобразованные секунды в стандартный формат времени: ";
+	cout << hour;
+	cout << ":";
+	cout << minute / 10 << minute % 10;
+	cout << ":";
+	cout << second / 10 << second % 10 << endl << endl;
+
+	cout << "Готово!" << endl;
+	cout << "Программа завершена." << endl;
+
+	return 0;
+}
