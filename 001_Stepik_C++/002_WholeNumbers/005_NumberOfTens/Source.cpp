5f45bbff35f47f5f988db06e5227595b4d8efe55
Artem <nik.artem.larin@gmail.com>
2021-03-12 21:14:03 +0600

    добавил NumberOfTens

---
+++ 001_Stepik_C++/002_WholeNumbers/005_NumberOfTens/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int number;
+
+	cout << "Введите положительное число: ";
+	cin >> number;
+
+	cout << "Число десятков в числе " << number;
+	cout << ": " << number % 100 / 10 << endl;
+
+	cout << endl << "Готово!" << endl;
+	cout << "Программа завершена." << endl;
+
+	return 0;
+}
