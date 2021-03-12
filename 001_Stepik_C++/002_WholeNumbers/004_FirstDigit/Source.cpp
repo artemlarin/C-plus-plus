ee96498f37e7e1bcd3a8ae0fc3e03eea287963c2
Artem <nik.artem.larin@gmail.com>
2021-03-12 21:13:25 +0600

    добавил FirstDigit

---
+++ 001_Stepik_C++/002_WholeNumbers/004_FirstDigit/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int number;
+
+	cout << "Введите двузначное число: ";
+	cin >> number;
+
+	cout << "Первая цифра числа " << number;
+	cout << " это: " << number / 10 << endl << endl;
+
+	cout << "Готово!" << endl;
+	cout << "Программа завершена." << endl;
+
+	return 0;
+}
