004528ebd056be871a4bd0b4c0697e611b3b3c47
Artem <nik.artem.larin@gmail.com>
2021-03-12 21:11:53 +0600

    добавил SquirrelsAndNuts

---
+++ 001_Stepik_C++/002_WholeNumbers/002_SquirrelsAndNuts/Source.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int squirrel, nut, remainder;
+
+	cout << "Введите количество белок: ";
+	cin >> squirrel;
+
+	cout << "Введите количество орешек найдеными белками: ";
+	cin >> nut;
+
+	remainder = nut % squirrel;
+
+	cout << endl << remainder;
+	cout << " Орешков останется после того, как все белочки возьмут себе равное количество орешков." << endl << endl;
+
+	cout << "Готово!" << endl;
+	cout << "Программа завершена." << endl;
+
+	return 0;
+}
