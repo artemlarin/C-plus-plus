888f600bc3a0ef5cb1140b492fafa75df8022a7c
Artem <nik.artem.larin@gmail.com>
2021-03-12 21:15:21 +0600

    добавил NextEven

---
+++ 001_Stepik_C++/002_WholeNumbers/007_NextEven/Source.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int number;
+
+	cout << "Введите целое положительное число: ";
+	cin >> number;
+
+	cout << "Следующее четное число за " << number;
+	cout << ": " << number / 2 * 2 + 2 << endl << endl;
+	//cout << ": " << (number / 2 + 1) * 2 << endl << endl;
+
+	cout << "Готово!" << endl;
+	cout << "Программа завершена." << endl << endl;
+
+	return 0;
+}
