ca9a94a90503cc53995a0df161c90eba04deb97b
Artem <nik.artem.larin@gmail.com>
2021-03-12 21:16:26 +0600

    добавил BuyingPies

---
+++ 001_Stepik_C++/002_WholeNumbers/009_BuyingPies/Source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int enteringRubles;
+	int inputOfKopecks;
+	int pies;
+
+	cout << "Введите цену одного пирожка в столовой." << endl;
+	cout << "Введите количество рублей: ";
+	cin >> enteringRubles;
+	cout << "Введите количество копеек: ";
+	cin >> inputOfKopecks;
+	cout << "Сколько пирожков вы хотите купить: ";
+	cin >> pies;
+
+	/* расчет рублей и копеек */
+	int rubles = pies * enteringRubles;
+	int penny = pies * inputOfKopecks;
+
+	/* общая сумма по рублям и копейкам */
+	int amountInRubles = penny / 100 + rubles;
+	int amountByKopecks = penny % 100;
+
+	cout << endl << "За " << pies << " пирожка Вам нужно заплатить: ";
+	cout << amountInRubles << " руб. " << amountByKopecks << " коп." << endl << endl;
+
+	/* Короткая форма записи */
+	/*enteringRubles = enteringRubles * pies;
+	inputOfKopecks = inputOfKopecks * pies;
+	cout << enteringRubles + inputOfKopecks / 100 << " " << inputOfKopecks % 100 << endl << endl;*/
+
+	cout << "Готово!" << endl;
+	cout << "Программа завершена." << endl << endl;
+
+	return 0;
+}
